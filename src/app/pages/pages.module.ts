d61ab080293efb87b53af2a4eaf2d4297474f74f
Reynaldo Cauche <reynaldocauche@gmail.com>
2020-04-03 06:07:53 -0500

    Agregamos y modificamos modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+
+// Modulos
+import { SharedModule } from '../shared/shared.module';
+
+// Rutas
+import { PAGES_ROUTES } from './pages.routes';
+
+// Componentes
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+
+@NgModule({
+    // tslint:disable-next-line: max-line-length
+    /* Cuando trabajamos con modulos puede ser que estas paginas solo nos sirvan para trabajar dentro de estas mismas paginas, pero en nuestro caso esta paginas tambien tiene que ser usadas por otros componentes que se encuentran fuera de este modulo por lo cual vamos a exportarlas */
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    // Ponemos todos los componentes o servicios que nosotros ocupamos que sean accesados desde fuera de este modulo
+    exports: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+       SharedModule,
+       PAGES_ROUTES
+    ]
+})
+
+export class PagesModule { }
---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,25 @@
+import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+
+const pagesRoutes: Routes = [
+    {
+        path: '',
+        component: PagesComponent,
+        // Defino hijos,Defino un arreglo secundario, con el router o roulet secundario
+        children: [
+            { path: 'dashboard', component: DashboardComponent},
+            { path: 'progress', component: ProgressComponent},
+            { path: 'graficas1', component: Graficas1Component},
+            // La ruta cuando no existe ninguna ruta
+            { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+        ]
+    },
+];
+
+// tslint:disable-next-line: max-line-length
+// El forRoot se usa cuando es la ruta principal, pero como estan son rutas que son rutas que estan dentro de otras rutas o mejor dicho router oulets dentro de otro router outlets por eso se usa el forChild
+export const PAGES_ROUTES = RouterModule.forChild( pagesRoutes );
